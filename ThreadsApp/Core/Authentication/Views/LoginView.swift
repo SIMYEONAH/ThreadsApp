d0425b76022402f1e0566130ce409da558bef612
Yeonah Shim <simya0528@naver.com>
2025-02-23 02:58:39 +0900

    Files

---
+++ ThreadsApp/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsApp
+//
+//  Created by 심연아 on 2/23/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
