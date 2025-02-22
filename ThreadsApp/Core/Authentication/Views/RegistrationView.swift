3e00b8425b9ed0196f00ffb0b4589e6051621c07
Yeonah Shim <simya0528@naver.com>
2025-02-23 03:35:26 +0900

    Login View Done

---
+++ ThreadsApp/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  ThreadsApp
+//
+//  Created by 심연아 on 2/23/25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
