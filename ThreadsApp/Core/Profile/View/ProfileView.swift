420dd445e31e37b061901c9776130bc6ccdf9b55
Yeonah Shim <simya0528@naver.com>
2025-02-27 22:28:19 +0900

    Making Profile View

--- ThreadsApp/Core/Profile/View/ProfileView.swift
+++ ThreadsApp/Core/Profile/View/ProfileView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View!")
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading, spacing: 12) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Pig Man")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text("porco_rosso")
+                        .font(.subheadline)
+                }
+            }
+        }
     }
 }
 
