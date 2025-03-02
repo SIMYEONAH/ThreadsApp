9b7e0dd78c77bd9ea83264bdd4311be8b6c2044d
Yeonah Shim <simya0528@naver.com>
2025-03-02 22:30:24 +0900

    Edit Profile View Start

---
+++ ThreadsApp/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  ThreadsApp
+//
+//  Created by 심연아 on 3/2/25.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
