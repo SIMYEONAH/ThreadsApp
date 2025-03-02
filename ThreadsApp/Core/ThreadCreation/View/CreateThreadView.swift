2697b30789a8eea840668aab494a1d8539a53762
Yeonah Shim <simya0528@naver.com>
2025-03-03 01:13:23 +0900

    Create Thread TextField

--- ThreadsApp/Core/ThreadCreation/View/CreateThreadView.swift
+++ ThreadsApp/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var caption = ""
+    
     var body: some View {
-        Text("Create Thread")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("pigman")
+                            .fontWeight(.semibold)
+                        
+                        TextField("What's on your mind?", text: $caption, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
